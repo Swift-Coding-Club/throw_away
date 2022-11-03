64d904e3c8cdc2445c6680dbe575889ba2bb9266
Juny0110 <wnsdyds403@naver.com>
2022-11-03 17:03:01 +0900

    #7 Refoctoring StatisticsView code

--- ThrowAway/ThrowAway/StatisticsView.swift
+++ ThrowAway/ThrowAway/StatisticsView.swift
@@ -12,50 +12,49 @@ struct StatisticsView: View {
     var lateThrowArray = ["Test01", "Test02", "Test03"]
     let setWidth = UIScreen.main.bounds.width
     
+    @EnvironmentObject var dateHolder: DateHolder
+    
     var body: some View {
+        
         VStack {
             Text("이번 달의 달성도는 81%입니다.")
                 .padding(.top, 20)
                 .font(.system(size: 20))
             
             // TODO: - Rectangle = 달력 위치
+            
             Rectangle()
                 .size(width: setWidth, height: setWidth)
             
-            countingProducts(text: "버리지 못한 물건", products: notThrowArray)
-            
-            productScroll(productList: notThrowArray)
-            
-            countingProducts(text: "늦게 버린 물건", products: lateThrowArray)
-            
-            productScroll(productList: lateThrowArray)
-//            Spacer()
+            ProductArrayView(text: "버리지 못한 물건", productArray: notThrowArray)
+            ProductArrayView(text: "늦게 버린 물건", productArray: lateThrowArray)
         }
     }
     
-    func countingProducts(text: String , products: [String]) -> some View {
-        HStack {
-            Text("\(text)(\(products.count))")
-                .font(.system(size: 18))
-                .fontWeight(.semibold)
-                .padding(.leading, 16)
-//                .padding(.top, 10)
-            Spacer()
+    func ProductArrayView(text: String , productArray: [String]) -> some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text("\(text)(\(productArray.count))")
+                    .font(.system(size: 18))
+                    .fontWeight(.semibold)
+                    .padding(.top, 10)
+                Spacer()
+            }
+            productScroll(productArray: productArray)
         }
+        .padding(.leading, 16)
     }
     
-    func productScroll(productList: [String]) -> some View {
+    func productScroll(productArray: [String]) -> some View {
         ScrollView(.horizontal) {
-            HStack{
-                ForEach(productList, id: \.self) {products in
-                    Image(products)
+            HStack {
+                ForEach(productArray, id: \.self) {product in
+                    Image(product)
                         .frame(width: setWidth / 6, height: setWidth / 6, alignment: .center)
                         .clipShape(Circle())
                 }
             }
         }
-        .padding(.leading, 18)
-//        .padding(.top, 8)
     }
 }
 
